e83929be88b1de55d0dd7ff3d3173a4e8250adea
TanishBansal189 <tanishbansal189@gmail.com>
2024-04-28 17:09:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function readmore(){
+document.getElementById('read-more').style.display='block'
+}
+// function know1(){
+// document.getElementById('read1').style.display='block'
+// }
+// console.log("hello world")
